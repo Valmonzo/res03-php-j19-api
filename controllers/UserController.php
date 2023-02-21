11b5a164728026b391d801abe8da95cb8f4a489a
Valmonzo <valmontpehautpietri@ip-172-31-22-161.eu-west-3.compute.internal>
2023-02-21 11:52:42 +0000

    Mise à jour du controller et du manager

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -11,36 +11,52 @@ class UserController extends AbstractController {
     public function getUsers()
     {
         // get all the users from the manager
+       $users =  $this->um->getAllUsers();
 
         // render
+        $this->render($users);
+
+
     }
 
     public function getUser(array $get)
     {
         // get the user from the manager
+        $user = $this->uc->getUserById($get['id']);
         // either by email or by id
 
         // render
+        $this->render(['user' => $user]);
     }
 
     public function createUser(array $post)
     {
         // create the user in the manager
+        $newUser = new User(null, $post['username'], $post['firstName'], $post['lastName'], $post['email']);
+        $userCreated = $this->uc->createUser($newUser);
 
         // render the created user
+        $this->render(['user'=> $userCreated]);
+
     }
 
     public function updateUser(array $post)
     {
         // update the user in the manager
+        $userToUpdate = new User($post['id'], $post['username'], $post['firstName'], $post['lastName'], $post['email']);
+        $this->uc->updateUser($userToUpdate);
 
         // render the updated user
+        $this->render(['user' => $userToUpdate]);
+
     }
 
     public function deleteUser(array $post)
     {
         // delete the user in the manager
+        $this->uc->deleteUser($post['id']);
 
         // render the list of all users
+        $this->render(['users' => $this->uc->getAllUsers()]);
     }
 }
--- managers/UserManager.php
+++ managers/UserManager.php
@@ -5,11 +5,32 @@ class UserManager extends AbstractManager {
     public function getAllUsers() : array
     {
         // get all the users from the database
+        $usersTab = [];
+
+        $query = $this->db->prepare('SELECT * FROM users');
+        $query->execute();
+        $users = $query->fetchAll(PDO::FETCH_ASSOC);
+        foreach($users as $user)
+        {
+            $userToPush = new User($user["id"], $user["username"], $user["firstName"], $user['lastName'], $user['email']);
+            $usersTab[] = $userToPush;
+        }
+
+        return $usersTab;
     }
 
     public function getUserById(int $id) : User
     {
         // get the user with $id from the database
+
+        $query = $this->db->prepare('SELECT * FROM users WHERE id = :id');
+        $parameters = [
+        'id' => $id
+        ];
+        $query->execute($parameters);
+        $user = $query->fetch(PDO::FETCH_ASSOC);
+
+        $userToLoad = new User($user['id'], $user['username'], $user['firstName'], $user['lastName'], $user['email']);
     }
 
     public function createUser(User $user) : User
@@ -17,6 +38,27 @@ class UserManager extends AbstractManager {
         // create the user from the database
 
         // return it with its id
+
+        $query = $this->db->prepare('INSERT INTO users (`id`,`username`, `firstName`, `lastName`, `email`) VALUES(NULL, :username, :firstName, :lastName, :email)');
+
+        $parameters = [
+        'email' => $user->getEmail(),
+        'username' => $user->getUsername(),
+        'firstName'=>$user->getFirstName(),
+        'lastName' =>$user->getLastName()
+        ];
+        $query->execute($parameters);
+
+        $query = $db->prepare('SELECT * FROM users WHERE email = :email');
+        $parameters = [
+            'email' => $user->getEmail()
+            ];
+        $query->execute($parameters);
+        $userSelected = $query->fetch(PDO::FETCH_ASSOC);
+        $userToLoad = new User($userSelected['id'], $userSelected['username'], $userSelected['firstName'], $userSelected['lastName'], $userSelected['email']);
+
+        return $userToLoad;
+
     }
 
     public function updateUser(User $user) : User
@@ -24,6 +66,19 @@ class UserManager extends AbstractManager {
         // update the user in the database
 
         // return it
+
+        $query = $this->db->prepare('UPDATE users SET username = :username, firstName = :firstName, lastName = :lastName, email = :email WHERE id = :id ');
+        $parameters = [
+            'id' => $user->getId(),
+            'email' => $user->getEmail(),
+            'username' => $user->getUsername(),
+            'firstName' => $user->getFirstName(),
+            'lastName' => $user->getLastName()
+            ];
+
+        $query->execute($parameters);
+
+        return $user;
     }
 
     public function deleteUser(User $user) : array
@@ -31,5 +86,14 @@ class UserManager extends AbstractManager {
         // delete the user from the database
 
         // return the full list of users
+
+        $query = $this->db->prepare('DELETE * FROM users WHERE id = :id ');
+        $parameters = [
+            'id'=> $user->getId()
+            ];
+
+        $query->execute($parameters);
+
+        return $this->getAllUsers();
     }
 }
